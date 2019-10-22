1df598aeca2c695b0c1963ac0961e63ae73071fa
ivanslavkomatic <48771316+ivanslavkomatic@users.noreply.github.com>
2019-10-22 22:07:23 +0200

    Added Home page, and main .css file. - Added multimedia folder and background image, edited a bit of design in main.css file.

---
+++ SoftwareLL/index.php
@@ -0,0 +1,38 @@
+<?php ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>SoftwareLL</title>
+        <meta charset = "UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+
+        <meta name="title" content="SoftwareLL">
+        <meta name="author" content="Ivan Slavko Matić">
+        <meta name="keywords" 
+              content="license, price, 
+              company">
+
+        <link href="css/main.css" 
+              rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header style="font-weight:bold">
+            <h1 id="headerID">Home</h1>
+        </header>
+        <br>
+        <nav>
+            <ul class="navigation">
+                <li><a href="ostalo/multimedija.html">Gallery</a></li>
+                <li><a href="ostalo/popis.html">Tables</a></li>   
+                <li><a href="obrasci/obrazac.html">Form</a></li> 
+                <li><a href="obrasci/prijava.html">Login</a></li> 
+                <li><a href="obrasci/registracija.html">Registration</a></li> 
+                <li><a href="navigacijski.html">Navigational diagram</a></li> 
+                <li><a href="era.html">ERA model</a></li> 
+            </ul>
+            <hr style="margin-left: 4%;">
+        </nav>
+    </body>
+</html>
