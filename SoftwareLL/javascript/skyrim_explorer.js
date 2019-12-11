1714f237c001cc5281050b96c055a9b5d9c9e677
ivanslavkomatic <48771316+ivanslavkomatic@users.noreply.github.com>
2019-12-11 12:13:06 +0100

    Added function preloader to javascript. Due to the load time of the image, white screen flashes would appear, preloader function loads the picture before the transitions, resulting in smoother experience.

--- SoftwareLL/javascript/skyrim_explorer.js
+++ SoftwareLL/javascript/skyrim_explorer.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     backgroundTrigger();
 
     function switchBackground() {
+        preloader();
         switch (background) {
             case 1:
                 $('body').css('background-image', 'url(../skyrim_source/backgrounds/back1.jpg)');
@@ -40,6 +41,25 @@ $(document).ready(function () {
                 background++;
                 switchBackground();
             }
-        }, 10000);
+        }, 2000);
+    }
+
+    //preloader --> helps with animation transitions (smoothing)
+    function preloader() {
+        if (document.images) {
+            var img1 = new Image();
+            var img2 = new Image();
+            var img3 = new Image();
+            var img4 = new Image();
+            var img5 = new Image();
+            var img6 = new Image();
+
+            img1.src = "../skyrim_source/backgrounds/back1.jpg";
+            img2.src = "../skyrim_source/backgrounds/back2.jpg";
+            img3.src = "../skyrim_source/backgrounds/back3.jpg";
+            img4.src = "../skyrim_source/backgrounds/back4.jpg";
+            img5.src = "../skyrim_source/backgrounds/back5.jpg";
+            img6.src = "../skyrim_source/backgrounds/back6.jpg";
+        }
     }
 });
